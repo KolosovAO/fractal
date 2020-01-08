9b6c51ae5a8c224a4d684673e4a70827145a0c78
akolosov <jora364@gmail.com>
2020-01-08 17:11:15 +0300

    Updates

--- sources/index.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from "./app";
-import "./styles/index.scss";
-
-document.addEventListener("DOMContentLoaded", () => {
-    const canvas = document.createElement("canvas");
-    document.body.appendChild(canvas);
-
-    new Application(canvas);
-});
--- sources/ts-utils/object-keys.ts
+++
@@ -1,1 +0,0 @@
-export const objectKeys = <O extends {}>(o: O) => Object.keys(o) as (keyof O)[];
